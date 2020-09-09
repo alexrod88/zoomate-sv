ae632625f38dc7a65ec1339a9adb0bbf277aaad2
Alex Rodriguez <alexrod88@outlook.com>
2020-09-09 07:43:34 +0000

    Adding neo4j configs and env setting

---
+++ zoomate/zoomate/settings/__init__.py
@@ -0,0 +1,11 @@
+from .base import *
+import os
+
+env_name = os.getenv('API_ENV', 'dev')
+
+if env_name == 'prod':
+    from .prod import *
+elif env_name == 'stage':
+    from .stage import *
+else:
+    from .dev import *
--- zoomate/zoomate/settings/base.py
+++ zoomate/zoomate/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '!7nu5@!jgr+v=jvb29rm_+r3hjw#w+c9k8oa6lg7uj$xy_hq4v'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+NEOMODEL_NEO4J_BOLT_URL = os.environ.get('NEO4J_BOLT_URL', 'bolt://neo4j:test@localhost:7687')
 
 # Application definition
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_neomodel',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +72,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'zoomate.wsgi.application'
 
+NEOMODEL_SIGNALS = True
+NEOMODEL_FORCE_TIMEZONE = False
+NEOMODEL_ENCRYPTED_CONNECTION = False
+NEOMODEL_MAX_POOL_SIZE = 50
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
